a7840f5e4c56b2bbd82a2ab15d939ca22999eb27
adrobic <aldin.drobic@hotmail.se>
2022-03-26 11:50:43 +0100

    FEAT: Added Db context

---
+++ RentalCarsApi/Data/RentalCarsDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentalCarsApi.Models;
+using System.Diagnostics.CodeAnalysis;
+
+namespace RentalCarsApi.Data
+{
+    public class RentalCarsDbContext: DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Price> Prices { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+
+        public RentalCarsDbContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
