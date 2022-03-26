1477e0e7e447ac78210102f16ecd13f9cb268f24
adrobic <aldin.drobic@hotmail.se>
2022-03-26 11:29:12 +0100

    FEAT: Added class "Car"

--- RentalCarsApi/Models/Car.cs
+++ RentalCarsApi/Models/Car.cs
@@ -2,6 +2,11 @@
 {
     public class Car
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Milage { get; set; }
+        public bool IsRented { get; set; }
+        public int CategoryId { get; set; }
+
     }
 }
