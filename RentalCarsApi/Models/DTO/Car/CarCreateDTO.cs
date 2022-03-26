914ec7bee8f985e57f9539d1947f2c750eff422e
adrobic <aldin.drobic@hotmail.se>
2022-03-26 20:46:49 +0100

    FEAT: Added DTOs for class car

---
+++ RentalCarsApi/Models/DTO/Car/CarCreateDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace RentalCarsApi.Models.DTO.Car
+{
+    public class CarCreateDTO
+    {
+        public string Name { get; set; }
+        public int Milage { get; set; }
+        public bool IsRented { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
---
+++ RentalCarsApi/Models/DTO/Car/CarReadDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace RentalCarsApi.Models.DTO.Car
+{
+    public class CarReadDTO
+    {
+        public string Name { get; set; }
+        public int Milage { get; set; }
+        public bool IsRented { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
